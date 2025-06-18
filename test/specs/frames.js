120f5e3ceb3fd65d01f76d59cae90163865b3b6a
alexvargas-31 <115881895+alexvargas-31@users.noreply.github.com>
2025-06-18 11:24:10 -0600

    Add files via upload
    Here are displayed the files for two tasks

---
+++ test/specs/frames.js
@@ -0,0 +1,58 @@
+const { expect } = require('@wdio/globals');
+const HomePage = require('../pageobjects/home.page')
+
+describe('Frames Test', () => {
+    it('Should go to the Frames page and check the title', async () => {
+        await HomePage.open();          // Abre la página principal
+        await HomePage.goToFrames();   // Hace clic en "Frames"
+
+        // Verifica que se muestra el título "Frames"
+        const title = await $('h3');
+        await title.waitForDisplayed();
+        const text = await title.getText();
+
+        expect(text).toBe('Frames');
+    });
+});
+
+describe('Nested Frames Test', () => {
+    it('Nested Frames and check MIDDLE and LEFT texts', async () => {
+        await HomePage.open();
+        await HomePage.goToFrames();
+        await HomePage.goToNestedFrames();
+
+        await browser.pause(2000); // pausa 2 segundos
+
+        // Cambiar al frame
+        browser.switchToFrame($("frame[name='frame-bottom']"))
+      //identify element with tagname
+      const buttomFrame = $('<body>')
+      //get text inside frame
+      console.log(buttomFrame.getText() + ' - Text inside frame')   
+      //switch to main page
+      browser.switchToFrame(null)
+      
+      browser.switchToFrame($("frame[name='frame-left']"))
+      //identify element with tagname
+      const leftFrame = $('<body>')
+      //get text inside frame
+      console.log(leftFrame.getText() + ' - Text inside frame')   
+      //switch to main page
+      browser.switchToFrame(null) 
+      
+      browser.switchToFrame($("frame[name='frame-middle']"))
+      //identify element with tagname
+      const middleFrame = $('<body>')
+      //get text inside frame
+      console.log(middleFrame.getText() + ' - Text inside frame')   
+      //switch to main page
+      browser.switchToFrame(null) 
+
+        // Regresar a la página anterior (frames)
+        await browser.back();
+
+        // Puedes agregar una pausa si quieres ver la página anterior
+        await browser.pause(2000);
+
+    });
+});
